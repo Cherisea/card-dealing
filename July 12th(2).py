7acf5ca291e16ec1f142c96209ff505d25669881
Joey Cherisea <PanHaiWJ@163.com>
2017-07-12 11:22:48 +0800

    Add files via upload

---
+++ July 12th(2).py
@@ -0,0 +1,7 @@
+values = list(range(1,11)) + 'Jack Queen King'.split()
+suits = 'diamonds, clubs, hearts, spades'.split()
+deck = ['%s of %s' %(v,s) for v in values for s in suits]
+from pprint import pprint
+from random import shuffle
+shuffle(deck)
+pprint(deck[: 12])
